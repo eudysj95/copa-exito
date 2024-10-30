d112ba3de61a1eba4991b56ae3d6e45a89b05b30
Eudys Mora <eudysjmorag@gmail.com>
2024-10-29 20:07:53 -0400

    correccion article

--- src/layout/PrecioViveres.jsx
+++ src/layout/PrecioViveres.jsx
@@ -68,11 +68,11 @@ export const PrecioViveres = () => {
               key={item.id}
               className="w-64 h-40 text-veryDarkBlue mb-4  border-2 shadow pt-1"
             >
-              <h3 className="ml-8 mt-6 text-xl font-bold">{item.producto}</h3>
-              <p className="ml-8 mt-2">
+              <h3 className="ml-3 mt-2 text-xl font-bold">{item.producto}</h3>
+              <p className="ml-3 mt-2">
                 Precio Dolares: {item.precioDolar.toFixed(3)} $
               </p>
-              <p className="ml-8 mt-2">
+              <p className="ml-3 mt-2">
                 Precio Bolivares: {(item.precioDolar * tasa).toFixed(3)} Bs
               </p>
             </article>
