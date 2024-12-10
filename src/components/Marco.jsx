5db788f7a886e29b7f2583f3d43bd1eeee30ae4b
Eudys Mora <74325384+eudysj95@users.noreply.github.com>
2024-12-10 18:06:58 -0400

    Actualizar Marco.jsx

--- src/components/Marco.jsx
+++ src/components/Marco.jsx
@@ -11,7 +11,7 @@ export function Marco({title, metodo}) {
   const [listado, setListado] = useState([]);
   const [busquedaState, setBusquedaState] = useState("");
 
-  const tasa = 49;
+  const tasa = 50;
 
   useEffect(() => {
     conseguirListado();
